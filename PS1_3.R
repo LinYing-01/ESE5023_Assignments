edf4d5e87cca1f953e38c198be283d51b9f649b2
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-10-18 22:19:12 +0800

    Update PS1_3.R
    try thinking about a problem more completely, and add the discusses of k==1 & k==2

--- PS1_3.R
+++ PS1_3.R
@@ -3,6 +3,8 @@ Pascal_triangle<-function(k){
   for(i in 1:k){
     Tri[i,c(1,i)]<-1
   }
+  # ignored the condition of k==1 & k==2
+  # your program can`t give a right answer when k==1 | k==2
   if(k>=1){
     for(i in 3:k){
       for(j in 2:(k-1)){
