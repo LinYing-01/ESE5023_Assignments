7f5b34cc372c804ec1ff933b6b07ea9c26761ccf
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-10-18 22:47:58 +0800

    Update PS1_4.R
    your program is unqualified for giving wrong results.
    please rewrite it with my annotations.
    MAKE SURE you can understand how to solve this problem.

--- PS1_4.R
+++ PS1_4.R
@@ -6,6 +6,10 @@ Least_moves<-function(x){
     }
     else if(2^i>x){
       m=i-1+x-2^(i-1)
+      # misunderstanding of this problem
+      # as a example of 55,the Least_move route must be "55>54>27>26>13>12>6>3>2>1", in 9 steps
+      # so halving is the fastest way to approaching 1, halving is the best option whenever it can be chosen
+      # rewrite this problem again
       break
     }
   }
