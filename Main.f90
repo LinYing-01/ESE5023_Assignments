29e3a13a8c03ee0da0ddb15ec90801d23b5effd2
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2021-01-08 19:52:19 +0800

    do note plagiarize!

--- Main.f90
+++ Main.f90
@@ -39,3 +39,9 @@ deallocate(a,b,c)
 
 end program Main
 
+! MingYANG noticed:
+! (1) in this question, M and N are not all in n×n
+! (2) using "matmul" can`t get the right answer
+! (3) please write the answer with the format of "(f8.1)"
+! (4) who is the plagiarist between you, Wangwang QIU, Chen DING and Haixiang ZENG?
+! the end
